8890c3bc49d9bbccfa64e4d36d4eaa84e0b6d81e
Ahmed-Mo-Eid <ahmed.eid@sherkety.net>
2026-05-16 19:38:39 +0300

    update env url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://chicken-distribution-backend.onrender.com/'
+  apiUrl: 'https://chicken-distribution-backend.onrender.com/api'
 };
